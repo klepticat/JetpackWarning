527900bff859121f31d085081cd32ec90a4c7d08
Kat <klepticat@gmail.com>
2023-12-03 17:56:41 -0500

    First Working Meter

--- JetpackWarning.cs
+++ JetpackWarning.cs
@@ -1,15 +1,83 @@
 ﻿using BepInEx;
 using UnityEngine;
+using HarmonyLib;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
-namespace JetpackWarning
-{
+namespace JetpackWarning {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
-    {
-        private void Awake()
-        {
-            // Plugin startup logic
+    public class JetpackWarningPlugin : BaseUnityPlugin {
+        public static Harmony _harmony;
+
+        private void Awake() {
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Assets.PopulateAssets();
+
+            _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            _harmony.PatchAll(typeof(Patches));
+
+            SceneManager.sceneLoaded += OnSceneRelayLoaded;
+        }
+
+        public static GameObject meterContainer, meter, frame, warning;
+
+        private void OnSceneRelayLoaded(Scene scene, LoadSceneMode loadMode) {
+            if(scene.name == "SampleSceneRelay") {
+                GameObject HUD = GameObject.Find("IngamePlayerHUD");
+
+                meterContainer = new GameObject("jetpackMeterContainer");
+
+                meterContainer.AddComponent<CanvasGroup>();
+
+                RectTransform containerTransform = meterContainer.AddComponent<RectTransform>();
+
+                containerTransform.parent = HUD.transform;
+                containerTransform.localScale = Vector3.one;
+                containerTransform.anchoredPosition = Vector2.zero;
+                containerTransform.localPosition = new Vector2(50, 0);
+                containerTransform.sizeDelta = Vector2.one;
+
+                meter = AddImageToHUD("jetpackMeter", scene);
+                frame = AddImageToHUD("jetpackMeterFrame", scene);
+                warning = AddImageToHUD("jetpackMeterWarning", scene);
+
+                GameObject[] meterObjects = { meter, frame, warning };
+                foreach(GameObject meterObject in meterObjects) {
+                    meterObject.transform.parent = meterContainer.transform;
+                    meterObject.transform.localPosition = Vector2.zero;
+                }
+
+                meter.GetComponent<Image>().type = Image.Type.Filled;
+                meter.GetComponent<Image>().fillMethod = Image.FillMethod.Vertical;
+
+                warning.transform.localPosition += new Vector3(30, 0);
+            }
+        }
+
+        private GameObject AddImageToHUD(string imageName, Scene scene) {
+            Sprite imageSprite = Assets.MainAssetBundle.LoadAsset<Sprite>(imageName);
+
+            GameObject imageObject = new GameObject(imageName);
+
+            SceneManager.MoveGameObjectToScene(imageObject, scene);
+
+            GameObject HUD = GameObject.Find("IngamePlayerHUD");
+
+            RectTransform imageTransform = imageObject.AddComponent<RectTransform>();
+            imageTransform.parent = HUD.transform;
+            imageTransform.localScale = Vector2.one;
+            imageTransform.anchoredPosition = Vector2.zero;
+            imageTransform.localPosition = Vector2.zero;
+            imageTransform.sizeDelta = new Vector2(imageSprite.rect.width/2, imageSprite.rect.height/2);
+
+            Image image = imageObject.AddComponent<Image>();
+            image.sprite = imageSprite;
+
+            CanvasRenderer meterImageCanvasRenderer = imageObject.AddComponent<CanvasRenderer>();
+
+            return imageObject;
         }
     }
 }
---
+++ Patches.cs
@@ -0,0 +1,28 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JetpackWarning {
+    class Patches {
+        [HarmonyPatch(typeof(PlayerControllerB), "LateUpdate")]
+        [HarmonyPostfix]
+        static void PlayerControllerB_LateUpdate_Postfix(ref PlayerControllerB __instance) {
+            if(__instance.isHoldingObject && __instance.currentlyHeldObjectServer != null && __instance.currentlyHeldObjectServer is JetpackItem) {
+                JetpackWarningPlugin.meterContainer.SetActive(true);
+            } else {
+                JetpackWarningPlugin.meterContainer.SetActive(false);
+            }
+        }
+
+        [HarmonyPatch(typeof(JetpackItem), "Update")]
+        [HarmonyPostfix]
+        static void JetpackItem_Update_Postfix(ref JetpackItem __instance, ref Vector3 ___forces, ref RaycastHit ___rayHit) {
+            float fill = ___forces.magnitude - ___rayHit.distance >= 0f ? (___forces.magnitude - ___rayHit.distance) / 50f : 0f;
+            JetpackWarningPlugin.meter.GetComponent<Image>().fillAmount = fill;
+        }
+    }
+}
