fa895d4946ef84eafd4f0d85e2341860a9a06ae2
Kat <klepticat@gmail.com>
2023-12-03 20:54:40 -0500

    Add colors to meter

--- Patches.cs
+++ Patches.cs
@@ -21,7 +21,13 @@ namespace JetpackWarning {
             if(__instance.heldByPlayerOnServer) {
                 float fill = ___forces.magnitude - ___rayHit.distance >= 0f ? (___forces.magnitude - ___rayHit.distance) / 50f : 0f;
                 JetpackWarningPlugin.meter.GetComponent<Image>().fillAmount = fill;
-                JetpackWarningPlugin.warning.SetActive((fill * 100f) > 75f);
+                JetpackWarningPlugin.warning.SetActive(fill > 0.85f);
+
+                Color meterColor = Color.Lerp(new Color(1f, 0.82f, 0.405f, 1f), new Color(0.769f, 0.243f, 0.243f, 1f), fill);
+
+                JetpackWarningPlugin.meter.GetComponent<Image>().color = meterColor;
+                JetpackWarningPlugin.frame.GetComponent<Image>().color = meterColor;
+                JetpackWarningPlugin.warning.GetComponent<Image>().color = meterColor;
             }
         }
     }
